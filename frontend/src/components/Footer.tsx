1c3376e970132d1da2e5419ea31daa4c27853862
thefidanabdulla <thefidanabdulla@gmail.com>
2024-05-14 14:45:17 +0400

    Main layout added on frontend side

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-blue-800 py-10'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <span className='text-3xl text-white font-semibold tracking-tight'>
+          MernHolidays.com
+        </span>
+        <p className='text-white font-bold tracking-tight flex gap-4'>
+          <span className='cursor-pointer'>Privacy Policy</span>
+          <span className='cursor-pointer'>Terms of Service</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
