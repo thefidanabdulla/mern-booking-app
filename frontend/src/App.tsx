112dad46a430b71832ad28119094ac8913f6ba88
thefidanabdulla <thefidanabdulla@gmail.com>
2024-05-15 12:40:09 +0400

    register form ui created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><p>Home PAGE</p></Layout>} />
         <Route path="/search" element={<Layout><p>Search PAGE</p></Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
