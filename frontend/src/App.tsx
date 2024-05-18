e30d4b6f4b0ef5b6fc842357ea7f830e9632ca2c
thefidanabdulla <thefidanabdulla@gmail.com>
2024-05-18 16:39:32 +0400

    sign in ui and fuctionality is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout><p>Home PAGE</p></Layout>} />
         <Route path="/search" element={<Layout><p>Search PAGE</p></Layout>} />
         <Route path="/register" element={<Layout><Register /></Layout>} />
+        <Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
