3046604ea6638c38273adb07fbb214c05ff793bb
thefidanabdulla <thefidanabdulla@gmail.com>
2024-05-25 12:38:14 +0400

    add hotel feature created. created test for add hotel and passed successfully e2e test

---
+++ frontend/src/forms/manageHotelForm/FacilitiesSection.tsx
@@ -0,0 +1,34 @@
+import { hotelFacilities } from "../../config/hotel-options-config";
+import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
+
+const FacilitiesSection = () => {
+  const { register, formState: { errors } } = useFormContext<HotelFormData>();
+  return (
+    <div className="">
+      <h2 className="text-2xl font-bold mb-3">Facilities</h2> 
+      <div className="grid grid-cols-5 gap-3">
+        {hotelFacilities.map((facility) => (
+          <label key={facility } className="flex items-center gap-1 text-sm">
+            <input type="checkbox" value={facility} {...register("facilities", {
+              validate: (facilities) => {
+                if(facilities && facilities.length > 0) {
+                  return true;
+                } else{
+                  return "At least one facility required"; 
+                }
+              }
+            })} />
+            {facility} 
+          </label>
+        ))}
+      </div>
+      {errors.facilities && (
+        <span className="text-red-400 text-xs">{errors.facilities.message}</span>
+      )}
+    </div>
+  )
+}
+
+export default FacilitiesSection;
+
