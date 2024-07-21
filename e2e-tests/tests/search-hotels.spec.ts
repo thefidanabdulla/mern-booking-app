d040905a5938f722f3197ed854660a259f5f136e
thefidanabdulla <thefidanabdulla@gmail.com>
2024-07-21 18:51:41 +0400

    hotel details page automated test

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -17,7 +17,7 @@ test.beforeEach(async ({ page }) => {
     await expect(page.getByText("Sign in successful!")).toBeVisible();
 });
 
-test("Should show hotel test results", async({ page }) => {
+test("should show hotel test results", async({ page }) => {
     await page.goto(UI_URL);
 
     await page.getByPlaceholder("Where are you going?").fill("Test City")
@@ -25,5 +25,16 @@ test("Should show hotel test results", async({ page }) => {
 
     await expect(page.getByText("Hotels found in Test City")).toBeVisible();
     await expect(page.getByText("Test Hotel")).toBeVisible();
+})
+
+test("should show hotel detail", async ({ page }) => {
+    await page.goto(UI_URL);
+
+    await page.getByPlaceholder("Where are you going?").fill("Test City")
+    await page.getByRole("button", { name: "Search" }).click();
+
+    await page.getByText("Test Hotel").click();
+    await expect(page).toHaveURL(/detail/);
+    await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
 })
 
